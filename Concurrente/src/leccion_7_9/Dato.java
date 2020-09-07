5bdb2f3b9e9b6c6882565077e5513ac711be8d25
Marcos <Marcos@LAPTOP-53OIQC5B>
2020-09-07 16:21:07 -0300

    creacion actividad 1 leccion del dia 7/09/2020. modificacion de la clase ping pong con una clase dato que cuenta la cantidad de iteraciones entre los hilos ping y pong

---
+++ Concurrente/src/leccion_7_9/Dato.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package leccion_7_9;
+
+/**
+ *
+ * @author Marcos
+ */
+public class Dato {
+   private int valor;
+
+void contar(){
+       valor = ++valor;
+}
+
+int obtenerValor(){
+       return valor;
+} 
+}
